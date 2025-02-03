95b660f6b38ee489896f8fb8d2278185d857f831
acs-web-tech <aruncsedeptjacsi@gmail.com>
2025-02-03 19:33:23 +0530

    Initial Template

---
+++ main/server.ts
@@ -0,0 +1,12 @@
+import net from "net";
+import BrokerEventHandler from "./BrokerModule/main";
+let initServer = net.createServer((socket)=>{
+   // Wrapper interface implemented here 
+   socket.on("close",(action)=>BrokerEventHandler.emit(action))
+   socket.on("connection",(action)=>BrokerEventHandler.emit(action))
+   //Not Implemented
+   //socket.on("data",(action)=>BrokerEventHandler.emitPayload(action))
+})
+initServer.listen(1883,()=>{
+   //Test
+})
