15689c1bcf4c54ba3387c8bac5cb0b3583ee0b44
acs-web-tech <aruncsedeptjacsi@gmail.com>
2025-02-06 00:29:35 +0530

    Params fix

--- main/server.ts
+++ main/server.ts
@@ -3,7 +3,7 @@ import {BrokerEventHandler} from "./BrokerModule/main";
 import { packets } from "./BrokerModule/Interfaces/Enums";
 let initServer = net.createServer((socket)=>{
    // Wrapper interface implemented here 
-   socket.on("close",(action)=>BrokerEventHandler.emit(action))
+   socket.on("close",(action)=>BrokerEventHandler.emit(action,socket))
    //Not Implemented
    socket.on("data",(action)=>{
    BrokerEventHandler.emitPayload(action,socket)
