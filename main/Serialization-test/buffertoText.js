b34a2cbbd639f378f96bef44f84d1b1e2dbc806e
P.ARUN <95969896+acs-web-tech@users.noreply.github.com>
2025-02-07 01:23:47 +0530

    Delete main/Serialization-test/buffertoText.js

--- main/Serialization-test/buffertoText.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var node_buffer_1 = require("node:buffer");
-var sourceBuffer = node_buffer_1.Buffer.from("hello", "utf-8");
-console.log(sourceBuffer.toString("utf-8"));
