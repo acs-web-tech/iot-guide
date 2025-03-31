f2fff87dc0164523a24bd9f6fb98eb2ac5feac8f
acs-web-tech <aruncsedeptjacsi@gmail.com>
2025-03-31 21:50:29 +0530

    connection disconnection due to sqlite error fixed

--- main/server.ts
+++ main/server.ts
@@ -1,10 +1,11 @@
 import * as net from "net";
 import { BrokerEventHandler } from "./BrokerModule/main";
 import { openDataBase,createTableDependency } from "./DBSqlite/init";
-let inMemory = openDataBase(":memory:")
-let onDisk = openDataBase("../Datastore/clients.db")
+let inMemory = openDataBase(":memory:");
+let onDisk = openDataBase("../Datastore/clients.db");
+(async()=>await createTableDependency({inMemory,onDisk}))();
 let initServer = net.createServer(async (socket) => {
-   let memory = await createTableDependency({inMemory,onDisk})
+  
    // Wrapper interface implemented here 
    let state = {}
    socket.on("close", (action) => BrokerEventHandler.emitPayload(action, socket,{onDisk,inMemory},state))
